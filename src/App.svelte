47a52639fe859a58d94eedfc230a6a921f27c8f7
JCalHij <38001598+JCalHij@users.noreply.github.com>
2023-07-24 16:03:53 +0200

    User Input and Data Binding

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,35 @@
 <script>
   let name = "Javi";
+  let inputName = "";
+
   const handleClick = () => {
-    name = "Javixu";
+    name = inputName;
   }
+
+  const handleInput = (e) => {
+    inputName = e.target.value;
+  }
+  
+  let syncedValue = "Mimi";
+  const handleSecondInput = (e) => {
+    syncedValue = e.target.value;
+  }
+
+  let bindValue = "Hello";
 </script>
 
 <main>
   <h1>My title</h1>
   <p>Hello {name}</p>
-  <button on:click={handleClick}>Click me to change the name</button>
+  <input value={inputName} type="text" on:input={handleInput}>
+  <button on:click={handleClick}>Update Name</button>
+  <br>
+  <br>
+  <p>Sync value: {syncedValue}</p>
+  <input value={syncedValue} type="text" on:input={handleSecondInput}>
+  <button on:click={()=>{syncedValue = name;}}>Another button</button>
+  <br>
+  <br>
+  <p>Sync value: {bindValue}</p>
+  <input bind:value={bindValue} type="text">
 </main>

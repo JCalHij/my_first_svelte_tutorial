07b602904101d49d70acf74a065066e950142f5e
JCalHij <38001598+JCalHij@users.noreply.github.com>
2023-07-25 15:27:53 +0200

    Forms 2

--- src/AddPersonForm.svelte
+++ src/AddPersonForm.svelte
@@ -2,16 +2,32 @@
     let name;
     let color;
     let age;
+    let isFighter = false;
+    let isHider = false;
+    let isFleer = false;
 
     const handleSubmit = () => {
-        console.log(name, color, age);
+        console.log(name, color, age, isFighter, isHider, isFleer);
     };
 </script>
 
 <form on:submit|preventDefault={handleSubmit}>
     <input type="text" placeholder="Name" bind:value={name} />
-    <input type="text" placeholder="Color" bind:value={color}/>
     <input type="number" placeholder="Age" bind:value={age}/>
+    <br>
+    <label>Skills:</label>
+    <input type="checkbox" bind:checked={isFighter}>Fighting<br>
+    <input type="checkbox" bind:checked={isHider}>Hiding<br>
+    <input type="checkbox" bind:checked={isFleer}>Fleeing<br>
+    <br>
+    <label>Colors:</label>
+    <select bind:value={color}>
+        <option value="black">Black</option>
+        <option value="red">Red</option>
+        <option value="blue">Blue</option>
+        <option value="orange">Orange</option>
+        <option value="green">Green</option>
+    </select>
     <button>Add Person</button>
 </form>
 
